93bed20dd370d1313db1675f89fb8d0b1a8697da
ezequieloscarescobar <ezequieloscarescobar2@gmail.com>
2016-04-17 19:21:17 -0300

    Primera version

---
+++ TpDDSAnual/TpDDSAnual/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpDDSAnual
+{
+    class Program
+    { static void Main(string[] args)
+        {
+        }
+    }
+
+    class Poi
+    {
+        //Atributos de la clase//
+        private string Nombre { get; set; }
+        private Coordenada coordenada;
+        private Direccion direccion;
+
+        //Setters y getters de los atributos//
+        //Structs de la clase//
+        public struct Coordenada
+        {
+          public float latitud;
+          public float longitud;
+          public Coordenada (float lat, float longi)
+            { latitud = lat;
+              longitud = longi; }
+         }
+        public struct Direccion
+        {
+            public int altura;
+            public string callePrincipal;
+            public Direccion(int alt, string calle)
+            {
+                altura = alt;
+                callePrincipal = calle;
+            }
+
+        }
+     }
+       
+ 
+    
+
+    
+}
