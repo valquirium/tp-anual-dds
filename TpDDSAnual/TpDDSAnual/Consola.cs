af2e738971493d6d0e2f71aecceb7bcd5cbc7f61
ezequieloscarescobar <ezequieloscarescobar2@gmail.com>
2016-05-17 10:35:02 -0300

    a

---
+++ TpDDSAnual/TpDDSAnual/Consola.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpDDSAnual
+{
+    class Consola
+    {
+        //ATRIBUTOS DE CLASE//
+        public Coordenada coordenada;
+        public int Comuna { get; set; }
+
+        //METODOS//
+        public bool consultarCercania(En)
+
+    }
+}
